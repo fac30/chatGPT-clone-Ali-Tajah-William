246dc6f445c4fbb9300bcfdcc1e3d023da73abb2
Whatsatajahism <tajah.hamilton@gmail.com>
2024-01-27 13:19:08 +0000

    relates #7

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-// Promises section //
-
-const ChatGPT = (key,callback) => {
-    fetch("https://api.openai.com/v1/chat/completions");
-    ChatGPT
-    .then(result => result.json())
-    .then(result => console.log('Key added'))
-    .then(data => {
-        callback(data)
-    })
-    .catch((error) => console.log(error));
-
-};
-
 //global variable to store API password//
-var masterKey 
+let masterKey = ""
 
 // variable to store API key//
 
@@ -30,5 +16,40 @@ document.getElementById("submit_key").addEventListener('click',
     event.preventDefault()
 } ) 
 
+//capture user input from form//
+
+let userInput = ""
+
+function userTextWriter(){
+    let userQuestion = document.getElementById('usertext').value
+    userInput =  userQuestion
+    console.log('user question asked')
+}
 
  
+// DOM to stop user submit button from reloading page//
+document.getElementById("user_submit").addEventListener('click', 
+(event) =>{
+    event.preventDefault()
+} ) 
+
+// Promises section //
+
+const chatGPT = (key,callback) => {
+
+    if (userInput || masterKey === ''){
+    console.log('password and/or input error')
+}
+
+    fetch("https://api.openai.com/v1/chat/completions")
+        .then(result => result.json())
+        .then(result => console.log('api info changed to json'))
+        .then(data => {
+        callback(data)
+    })
+    .catch((error) => console.log(error));
+ 
+
+};
+
+
